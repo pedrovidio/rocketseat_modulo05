e715f24977c8ebd8368ab02881c4918a50ae5148
Pedro Ovidio <pedrovidio@gmail.com>
2020-01-07 17:31:08 -0300

    estilizacao lista repositorio

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,11 +1,43 @@
 import React, { Component } from 'react';
+import { FaGithubAlt, FaPlus, FaSpinner } from 'react-icons/fa';
 
-import { FaGithubAlt, FaPlus } from 'react-icons/fa';
+import api from '../../services/api';
 
-import { Container, Form, SubmitButton } from './styles';
+import { Container, Form, SubmitButton, List } from './styles';
 
 export default class Main extends Component {
+  state = {
+    newRepo: '',
+    repositories: [],
+    loading: false,
+  };
+
+  handleInputChange = e => {
+    this.setState({ newRepo: e.target.value });
+  };
+
+  handleSubmit = async e => {
+    e.preventDefault();
+
+    this.setState({ loading: true });
+
+    const { newRepo, repositories } = this.state;
+
+    const response = await api.get(`/repos/${newRepo}`);
+
+    const data = {
+      name: response.data.full_name,
+    };
+
+    this.setState({
+      repositories: [...repositories, data],
+      newRepo: '',
+      loading: false,
+    });
+  };
+
   render() {
+    const { newRepo, repositories, loading } = this.state;
     return (
       <Container>
         <h1>
@@ -13,13 +45,31 @@ export default class Main extends Component {
           Repositórios
         </h1>
 
-        <Form onSubmit={() => {}}>
-          <input type="text" placeholder="Adicionar repositório" />
+        <Form onSubmit={this.handleSubmit}>
+          <input
+            type="text"
+            placeholder="Adicionar repositório"
+            value={newRepo}
+            onChange={this.handleInputChange}
+          />
 
-          <SubmitButton>
-            <FaPlus color="#fff" size={14} />
+          <SubmitButton loading={loading}>
+            {loading ? (
+              <FaSpinner color="#fff" size={14} />
+            ) : (
+              <FaPlus color="#fff" size={14} />
+            )}
           </SubmitButton>
         </Form>
+
+        <List>
+          {repositories.map(repository => (
+            <li key={repository.name}>
+              <span>{repository.name}</span>
+              <a href="#">Detalhes</a>
+            </li>
+          ))}
+        </List>
       </Container>
     );
   }
